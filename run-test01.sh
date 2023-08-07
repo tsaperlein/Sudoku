19faaa314bc29038670556e9b177a380d39ed2b5
Alex Tsaparas <up1072824@upnet.gr>
2023-08-07 14:46:40 +0300

    Script takes .in file and creates .out file

--- run-test01.sh
+++ run-test01.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-# Check if the sudoku-solver executable exists
-if [ ! -f sudoku-solver ]; then
-    echo "sudoku-solver executable not found."
-    exit 1
-fi
+while true; do
+    read -p "Enter the name of a .in file (or 'exit' to quit): " input_name
 
-if [ ! -f tests ]; then
-    echo "tests directory not found."
-    exit 1
-fi
+    if [ "$input_name" = "exit" ]; then
+        echo "Exiting the script."
+        break
+    fi
 
-echo "5x5 puzzle test"
-./sudoku-solver << EOF
-$(cat tests/5x5_test.in)
-EOF
+    input_file="$input_name.in"
+
+    if [ ! -f "tests/$input_file" ]; then
+        echo "File $input_file not found."
+    else
+        # Check if the sudoku-solver executable exists
+        if [ ! -f sudoku-solver ]; then
+            echo "sudoku-solver executable not found."
+            exit 1
+        fi
+
+        solution=$(./sudoku-solver < "tests/$input_file")
+
+        echo -e "\n- Running $input_file"
+        echo -e "Solution:"
+        echo "$solution"
+
+        output_file="${input_file%.in}.out"
+        if [ ! -f "tests/$output_file" ]; then
+            echo -e "\nFile $output_file not found, so it is created."
+        fi
+
+        echo "$solution" > "tests/$output_file"
+        echo -e " "
+    fi
+done
